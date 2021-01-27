294fe26a5f63739d0720f4e70f04faf78d187a8d
sushant <ksushant6566@gmail.com>
2021-01-27 23:27:56 +0530

    Intersection of LinkedList approach 1(equal travel approach)

---
+++ src/dsa/IntersectionOfLinkedLists.java
@@ -0,0 +1,30 @@
+package dsa;
+
+public class IntersectionOfLinkedLists {
+	class ListNode {
+		ListNode next;
+		int data;
+		
+		ListNode(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode tailA = headA;
+		ListNode tailB = headB;
+		
+		while(headA != null && headB != null) {
+			if(headA == headB) return headB;
+			
+			if(tailB == null) headA = headA.next;
+			else tailB = tailB.next;
+			
+			if(tailA == null) headB = headB.next;
+			else tailA = tailA.next;
+			
+		}
+		return null;
+	}
+}
